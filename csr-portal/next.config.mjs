7f98c36446e39547a6d72b56ff19449389be8d84
Pranav Kumar Varshney <vpranav2310@gmail.com>
2026-04-13 10:06:11 +0530

    API added for hosting

--- csr-portal/next.config.mjs
+++ csr-portal/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output:'export',
+  basePath: '/csr-portal',
+  assetPrefix: '/csr-portal/',
+  images:
+  {
+    unoptimized: true,
+  },
   reactCompiler: true,
 };
 
