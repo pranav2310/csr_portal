78692495bd151dc7c2ba172f65eccca6b9f492ad
Pranav Kumar Varshney <vpranav2310@gmail.com>
2026-04-13 10:01:26 +0530

    Updates

--- _next/static/-5P9Ufc7PLzUC8n5qkHIJ/_buildManifest.js
+++ _next/static/-5P9Ufc7PLzUC8n5qkHIJ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/csr-portal//_next/:path+",
+        "destination": "/csr-portal/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
